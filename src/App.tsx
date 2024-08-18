072e564117719cb854897dbe1a86feb9a2fff150
svbygoibear <svbyogibear@gmail.com>
2024-08-18 22:03:32 +0200

    :pencil: Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { LoadingAnimation } from "./components/LoadingAnimation/LoadingAnimation";
 
 export const App: React.FunctionComponent = () => {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <LoadingAnimation />
