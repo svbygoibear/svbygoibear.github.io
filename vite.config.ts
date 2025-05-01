9e5777a4aa34a0c57c8a2d0476e3fd304b7f8748
svbygoibear <svbyogibear@gmail.com>
2025-05-02 01:23:18 +0200

    Minor vite update.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "/"
-    // build: {
-    //     rollupOptions: {
-    //         input: {
-    //             main: "index.html"
-    //         },
-    //         output: {
-    //             assetFileNames: assetInfo => {
-    //                 if (assetInfo?.name?.endsWith(".pdf")) {
-    //                     return "[name][extname]";
-    //                 }
-    //                 return "assets/[name]-[hash][extname]";
-    //             }
-    //         }
-    //     }
-    // }
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "src/assets/*.json",
+                    dest: "assets"
+                }
+            ]
+        })
+    ],
+    base: "/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html"
+            },
+            output: {
+                assetFileNames: assetInfo => {
+                    if (assetInfo?.name?.endsWith(".pdf")) {
+                        return "[name][extname]";
+                    }
+                    return "assets/[name]-[hash][extname]";
+                }
+            }
+        }
+    }
 });
