2f868ba0e12c7175403f6e2a5ce2e019abc6d8ea
svbygoibear <svbyogibear@gmail.com>
2025-05-02 01:15:35 +0200

    Another test. :beer:

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/",
-    build: {
-        rollupOptions: {
-            input: {
-                main: "index.html"
-            },
-            output: {
-                assetFileNames: assetInfo => {
-                    if (assetInfo?.name?.endsWith(".pdf")) {
-                        return "assets/[name][extname]";
-                    }
-                    return "assets/[name]-[hash][extname]";
-                }
-            }
-        }
-    }
+    base: "/"
+    // build: {
+    //     rollupOptions: {
+    //         input: {
+    //             main: "index.html"
+    //         },
+    //         output: {
+    //             assetFileNames: assetInfo => {
+    //                 if (assetInfo?.name?.endsWith(".pdf")) {
+    //                     return "[name][extname]";
+    //                 }
+    //                 return "assets/[name]-[hash][extname]";
+    //             }
+    //         }
+    //     }
+    // }
 });
